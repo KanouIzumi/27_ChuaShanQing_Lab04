0a0e445350e48381a5615c1948b9a555466a1f99
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2022-01-20 16:03:27 +0800

    Player and coin added
    Player is able to move and the coin is able to rotate

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody PlayerRigidbody;
+
+    public static int coinCount;
+    public int totalCoinCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRigidbody = GetComponent<Rigidbody>();
+
+        totalCoinCount = GameObject.FindGameObjectsWithTag("Coin").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        PlayerRigidbody.AddForce(movement * speed * Time.deltaTime);
+
+        //Win condition
+        if (coinCount == totalCoinCount)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Coin")
+        {
+            print("Get coin!");
+            Destroy(collision.gameObject);
+        }
+    }
+}
